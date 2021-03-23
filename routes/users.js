1a48d1bf99f143b66939931545b6fc40d3feebdb
sandeepnadendla-git <77738760+sandeepnadendla-git@users.noreply.github.com>
2021-03-23 01:30:41 -0500

    user.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var num=0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  num++;
+  res.send('User accesses are: '+num);
 });
 
 module.exports = router;
+
+
