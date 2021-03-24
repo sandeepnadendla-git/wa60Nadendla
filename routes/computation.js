21aca9f8ab00c9e75c2fe88bd8481715a11e7b3f
sandeepnadendla-git <77738760+sandeepnadendla-git@users.noreply.github.com>
2021-03-24 17:11:56 -0500

    bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
-var num = Math.floor(Math.random()*100);
+
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.send('cbrt applied to ' + num + ' is ' + Math.cbrt(num));
+    var num = Math.floor(Math.random() * 100);
+
+    fake_url = "https://fake.com/path" + req.url
+    const url = new URL(fake_url)
+    const search_params = url.searchParams;
+    let x = search_params.get("x");
+    if(x==null||x==""){
+
+        res.send('cbrt applied to ' + num + ' is ' + Math.cbrt(num));
+    }
+    else{
+        res.send('cbrt applied to ' + x + ' is ' + Math.cbrt(x));
+        
+    }
+    console.log(x);
+
 });
+
 
 module.exports = router;
 
